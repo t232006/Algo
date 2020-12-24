af1b14eff3e781ca777940c867593d394026d94f
Тимофей Приходько <t232006@yandex.ru>
2020-12-25 00:22:22 +0500

    неполадки с исходным массивом

---
+++ task3/task3/task3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    
+    class Program
+    {
+        static byte[] MyMas = { 8, 3, 5, 1, 2, 4, 3, 8, 2, 6, 8, 9, 2, 6, 3, 7, 2, 6, 5, 7, 8, 9, 5, 8, 9, 8, 5 };
+
+        delegate byte[] Func(byte[] mas);
+        static void ToString(byte[] mas)
+        {
+            foreach (byte i in mas)
+                Console.Write($"{i}, ");
+            Console.WriteLine();
+        }
+
+        static string TimeCounter(Func Sort)
+        {
+            DateTime dt =DateTime.Now;
+            ToString(MyMas);
+            ToString(Sort(MyMas));
+            ToString(MyMas);
+            return "Прошло миллисекунд: "+(DateTime.Now.Millisecond - dt.Millisecond).ToString();
+        }
+        
+        static void Main(string[] args)
+        {
+            bubleSort bs = new bubleSort();
+            
+            Console.WriteLine(TimeCounter(bs.bubbleSortS));
+            //Console.WriteLine(TimeCounter(bs.bubbleSortUp));
+            //Console.WriteLine(TimeCounter(bs.Shaiker));
+
+            //ToString(bs.bubbleSortS(mas));
+            //ToString(bs.bubbleSortUp(mas));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ task3/task3/task3/bubleSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+//    Попробовать оптимизировать пузырьковую сортировку.Сравнить количество операций сравнения
+//оптимизированной и не оптимизированной программы. Написать функции сортировки, которые
+//возвращают количество операций.
+    class bubleSort
+    {
+        void swap(ref byte a, ref byte b)
+        {
+            a = (byte)(a + b);
+            b = (byte)(a - b);
+            a = (byte)(a - b);
+        }
+        public byte[] bubbleSortS(byte[] mas1)
+        {
+            byte[] mas = mas1;
+            for (int i = mas.Length-1; i > 0; i--)
+                for (int j = mas.Length-1; j > 0; j--)
+                    if (mas[j] < mas[j - 1]) swap(ref mas[j], ref mas[j - 1]);
+            return mas;
+        }
+        public byte[] bubbleSortUp(byte[] mas1)
+        {
+            byte[] mas = mas1;
+            for (int i = mas.Length - 1; i > 0; i--)
+                for (int j = mas.Length - 1; j > mas.Length-i; j--)
+                    if (mas[j] < mas[j - 1]) swap(ref mas[j], ref mas[j - 1]);
+            return mas;
+        }
+        public byte[] Shaiker(byte[] mas1)
+        {
+            byte[] mas = mas1;
+            for (int i = 0; i > mas.Length; i++)
+                for (int j = 0; j > mas.Length; j++)
+                    if (mas[j] > mas[j - 1]) swap(ref mas[j], ref mas[j - 1]);
+            return mas;
+        }
+    }
+}
