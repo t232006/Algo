e7dcedd744c3ed28ed659da78eb27e29b59ecf2f
Tim <t232006@yandex.ru>
2021-03-09 16:56:17 +0300

    intermidiate

--- labirint/labirint/labirint/Program.cs
+++ labirint/labirint/labirint/Program.cs
@@ -16,18 +16,18 @@ namespace labirint
                 public short Y;
                 public short state;
             }
-            static byte rows = 19;
-            static byte cols = 49;
+            static byte rows = 7;
+            static byte cols = 7;
             public OneCell[,] map = new OneCell[rows, cols];
             List<OneCell> candidat = new List<OneCell>();
             List<OneCell> getCand(OneCell current)
             {
                 List<OneCell> tempcand = new List<OneCell>();
-                if (current.X < cols - 1 && map[current.X + 2, current.Y].state == 0) tempcand.Add(map[current.X + 2, current.Y]);
-                if (current.X > 2 && map[current.X - 2, current.Y].state == 0) tempcand.Add(map[current.X - 2, current.Y]);
-                
-                if (current.Y < rows - 1 && map[current.Y, current.Y + 2].state == 0) tempcand.Add(map[current.X, current.Y + 2]);
-                if (current.X > 2 && map[current.Y, current.Y - 2].state == 0) tempcand.Add(map[current.Y, current.Y - 2]);
+ /*r*/          if (current.X < cols - 2 && map[current.X + 2, current.Y].state == 0) tempcand.Add(map[current.X + 2, current.Y]);
+ /*l*/          if (current.X > 2 && map[current.X - 2, current.Y].state == 0) tempcand.Add(map[current.X - 2, current.Y]);
+              
+ /*d*/          if (current.Y < rows - 2 && map[current.X, current.Y + 2].state == 0) tempcand.Add(map[current.X, current.Y + 2]);
+ /*u*/          if (current.Y > 2 && map[current.X, current.Y - 2].state == 0) tempcand.Add(map[current.X, current.Y - 2]);
                 return tempcand;
             }
             void DestroyWall (OneCell current, short counter)
@@ -38,40 +38,23 @@ namespace labirint
                 if (tempcand.Count != 0)
                 {
                     cand = tempcand[step.Next(tempcand.Count)];
+                    map[current.X,current.Y].state = counter++;
                     if (current.X == cand.X)
-                        if (current.Y > cand.Y)
-                        {
-                            map[current.X, current.Y - 1].state = counter;
-                            //DestroyWall(map[current.X, current.Y - 1], counter++);
-                        }
-                        else
-                        {
-                            map[current.X, current.Y + 1].state = counter;
-                            //DestroyWall(map[current.X, current.Y + 1], counter++);
-                        }
-
+                        if (current.Y > cand.Y) map[current.X, current.Y - 1].state = counter; else 
+                                                map[current.X, current.Y + 1].state = counter;
                     else
-                        if (current.X > cand.X) 
-                        { 
-                            map[current.X - 1, current.Y].state = counter;
-                            //DestroyWall(map[current.X - 1, current.Y], counter++);
-                        }
-                        else 
-                        {
-                            map[current.X + 1, current.Y].state = counter;
-                            //DestroyWall(map[current.X + 1, current.Y], counter++);
-                        }
-                    DestroyWall(cand, counter++);
+                        if (current.X > cand.X) map[current.X - 1, current.Y].state = counter; else
+                                                map[current.X + 1, current.Y].state = counter;
+                    DestroyWall(cand, ++counter);
                 }  
             }
             public Tmap(short X_,short Y_)
             {
-                OneCell OC;
-                OC.X = X_; OC.Y = Y_; OC.state = 0;
                 InitMap();
+                //map[X_, Y_].state = 1;
+                OneCell OC=map[X_,Y_];
                 DestroyWall(OC,1);
             }
-
             public void printMap()
             {
                 for (int i=0; i<rows; i++)
@@ -96,9 +79,7 @@ namespace labirint
                         else map[i, j].state = -1;
                     }
             }
-
-        }
-       
+        }  
         static void Main(string[] args)
         {
             Tmap map1 = new Tmap(1,1);
